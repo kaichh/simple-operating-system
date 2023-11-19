3f1725169900ba38b1270e8a0f1f66c746ade0df
kaichh <huangkc3@gmail.com>
2023-11-19 08:35:01 -0600

    MP6: Report

--- mp6/MP6_Sources/blocking_disk.C
+++ mp6/MP6_Sources/blocking_disk.C
@@ -22,13 +22,17 @@
 #include "utils.H"
 #include "console.H"
 #include "blocking_disk.H"
+#include "scheduler.H"
+
+extern Scheduler * SYSTEM_SCHEDULER;
 
 /*--------------------------------------------------------------------------*/
 /* CONSTRUCTOR */
 /*--------------------------------------------------------------------------*/
 
-BlockingDisk::BlockingDisk(DISK_ID _disk_id, unsigned int _size) 
-  : SimpleDisk(_disk_id, _size) {
+BlockingDisk::BlockingDisk(DISK_ID _disk_id, unsigned int _size) {
+  disk_id = _disk_id;
+  disk_size = _size;
 }
 
 /*--------------------------------------------------------------------------*/
@@ -36,13 +40,69 @@ BlockingDisk::BlockingDisk(DISK_ID _disk_id, unsigned int _size)
 /*--------------------------------------------------------------------------*/
 
 void BlockingDisk::read(unsigned long _block_no, unsigned char * _buf) {
-  // -- REPLACE THIS!!!
-  SimpleDisk::read(_block_no, _buf);
+  /* Reads 512 Bytes in the given block of the given disk drive and copies them 
+   to the given buffer. No error check! */
+
+  issue_operation(DISK_OPERATION::READ, _block_no);
+
+  wait_until_ready();
+
+  /* read data from port */
+  int i;
+  unsigned short tmpw;
+  for (i = 0; i < 256; i++) {
+    tmpw = Machine::inportw(0x1F0);
+    _buf[i*2]   = (unsigned char)tmpw;
+    _buf[i*2+1] = (unsigned char)(tmpw >> 8);
+  }
 
 }
 
 
 void BlockingDisk::write(unsigned long _block_no, unsigned char * _buf) {
-  // -- REPLACE THIS!!!
-  SimpleDisk::write(_block_no, _buf);
+  /* Writes 512 Bytes from the buffer to the given block on the given disk drive. */
+
+  issue_operation(DISK_OPERATION::WRITE, _block_no);
+
+  wait_until_ready();
+
+  /* write data to port */
+  int i; 
+  unsigned short tmpw;
+  for (i = 0; i < 256; i++) {
+    tmpw = _buf[2*i] | (_buf[2*i+1] << 8);
+    Machine::outportw(0x1F0, tmpw);
+  }
+}
+
+bool BlockingDisk::is_ready() {
+   return ((Machine::inportb(0x1F7) & 0x08) != 0);
+}
+
+void BlockingDisk::wait_until_ready() {
+  while(!is_ready()) {
+    Console::puts("The disk is not ready! Giving up CPU to another thread...\n");
+    SYSTEM_SCHEDULER->resume(Thread::CurrentThread());
+    SYSTEM_SCHEDULER->yield();
+  }
+}
+
+// Same function as in SimpleDisk
+void BlockingDisk::issue_operation(DISK_OPERATION _op, unsigned long _block_no) {
+
+  Machine::outportb(0x1F1, 0x00); /* send NULL to port 0x1F1         */
+  Machine::outportb(0x1F2, 0x01); /* send sector count to port 0X1F2 */
+  Machine::outportb(0x1F3, (unsigned char)_block_no);
+                         /* send low 8 bits of block number */
+  Machine::outportb(0x1F4, (unsigned char)(_block_no >> 8));
+                         /* send next 8 bits of block number */
+  Machine::outportb(0x1F5, (unsigned char)(_block_no >> 16));
+                         /* send next 8 bits of block number */
+  unsigned int disk_no = disk_id == DISK_ID::MASTER ? 0 : 1;
+  Machine::outportb(0x1F6, ((unsigned char)(_block_no >> 24)&0x0F) | 0xE0 | (disk_no << 4));
+                         /* send drive indicator, some bits, 
+                            highest 4 bits of block no */
+
+  Machine::outportb(0x1F7, (_op == DISK_OPERATION::READ) ? 0x20 : 0x30);
+
 }
